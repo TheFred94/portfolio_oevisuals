18eec7aec1048efe8fbba812e4b45a0502d8a119
TheFred94 <frederikrlarsen@gmail.com>
2023-02-28 16:46:56 +0100

    passion removed. Not working. Need new solution

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 const toggleMenu = () => {
   document.body.classList.toggle("open");
 };
+
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true,
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
